d4b370bdbdfe7d7d7a27b348245a2d798062a708
Zetius360 <qmax360@gmail.com>
2021-04-24 13:28:21 +0300

    first commit

---
+++ js/123.js
@@ -0,0 +1,7 @@
+;(function () {
+  const menus = document.querySelector('.menu-btn')
+  const menuur = document.querySelector('.menu-nav')
+  menus.addEventListener('click', () => {
+    menus.classList.toggle('active')
+  })
+})()
